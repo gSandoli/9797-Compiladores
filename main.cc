e83984543ec853f3d78a5da3a1895ab3a7b06c64
Diogo Almeida <diogo.almeida@olxbr.com>
2022-04-18 00:16:15 -0300

    Working with args from main

--- main.cc
+++ main.cc
@@ -5,23 +5,66 @@
 #include "driver.hh"
 using namespace std;
 
+static void usage()
+{
+  fprintf(stderr, "Usage: %s [-n number][-p pagefile] trace n1 n2 ...\n");
+  exit(EXIT_FAILURE);
+}
+
 int main(int argc, char **argv)
 {
   string filename;
+  string output = "a.out";
+  bool intermediateCode = false;
+  bool assemblyCode = false;
   int opt;
-  while ((opt = getopt(argc, argv, "i:?")) != EOF)
+
+  if (argc == 1)
+  {
+    cerr << "ERROR: missing arguments" << endl;
+    cout << "USAGE: ./sc [-i] [-s] [-o <destino>] <fonte>" << endl;
+    cout << "\t-i : imprime o código intermediário. O arquivo de saída deve chamar fonte.ll // opcional" << endl;
+    cout << "\t-s : imprime o código Assembly. O arquivo de saída deve chamar fonte.s // opcional" << endl;
+    cout << "\t-o <destino> : gera o executável com o nome destino. Caso não seja fornecido, deve ser gerado um arquivo chamado a.out. // optional" << endl;
+    cout << "\t<fonte> : código fonte, cujo extensão é .s" << endl;
+    abort();
+  }
+
+  while ((opt = getopt(argc, argv, "iso:")) != EOF)
+  {
     switch (opt)
     {
     case 'i':
-      filename = optarg;
+      intermediateCode = true;
       break;
-    case '?':
-      cerr << "Usuage: tc -i <filename>" << endl;
+    case 's':
+      assemblyCode = true;
+      break;
+    case 'o':
+      output = optarg;
+      output += ".out";
       break;
     default:
       cerr << endl;
       abort();
     }
+  }
+
+  int argsNotParsed = argc - optind;
+  if (argsNotParsed < 1)
+  {
+    cerr << "ERROR: source file is missing" << endl;
+    abort();
+  }
+  else if (argsNotParsed > 1)
+  {
+    cerr << "ERROR: source file is wrong" << endl;
+    abort();
+  }
+  else
+  {
+    filename = argv[optind];
+  }
 
   Simples::Driver driver;
   driver.parse_file(filename);
--- test2.s
+++ test2.s
@@ -1,2 +1,6 @@
 ação:
-    imprimei(10)
+    imprimei(1);
+    func1(2);
+    func2(3);
+    func3(4);
+    func4(5)
